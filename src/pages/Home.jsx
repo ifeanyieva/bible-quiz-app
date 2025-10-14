5d220574f7fb18de98ad634b803e1d431869dede
ifeanyieva <evailomuanya95@gmail.com>
2025-10-14 23:14:57 +0100

    made some corrections in home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,23 +3,9 @@ import VerseLookupForm from "../components/VerseLookupForm";
 
 const Home = () => {
   return (
-    <div>
+    <div className="p-6">
       <DailyVerse />
       <VerseLookupForm />
-    </div>
-  );
-};
-
-export default Home;
-import DailyVerse from "../components/DailyVerse";
-
-const Home = () => {
-  return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold text-center mb-4 text-blue-700">
-        Welcome to the Bible Quiz App
-      </h1>
-      <DailyVerse />
     </div>
   );
 };
